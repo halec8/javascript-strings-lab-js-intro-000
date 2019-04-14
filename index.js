032d41ae0b43e017f25bd793db99afa6596b70bf
Cody Hale <hale.l.c91@gmail.com>
2019-04-14 02:58:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-var greeting = "";
+var greeting = "Hiya";
 
 var specialGuest = "Neil deGrasse Tyson"
 
-var greetSpecialGuest = "" + specialGuest + "!";
+var greetSpecialGuest = "Hiya" + specialGuest + "!";
 
 var topic = "space";
 
